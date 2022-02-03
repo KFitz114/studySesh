dad0417f7f71d663545113a229b04bc9c6ff7fcc
KFitz114 <flipperly@protonmail.com>
2022-02-02 23:18:50 -0500

    added credit for background images

--- front/flashcards/src/Components/About.js
+++ front/flashcards/src/Components/About.js
@@ -9,6 +9,8 @@ class About extends React.Component {
                     The flash cards created are sent to and retreived from a Mongo Database using the Node.js server model.
                 </p>
             </div>
+            <p> Credit for randomized back-ground images goes to freepik.com </p>
+                <a href="https://www.freepik.com/vectors/business">Business vector created by rawpixel.com - www.freepik.com</a>
         </div>
     }   
 }
